715a8e97e92dd27310d684af170d8aab405e006d
Nathan <yoon.nathan@gmail.com>
2021-04-22 09:27:27 -0700

    Banking class rough outline implemented

---
+++ Banking Model/Bank.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  Banking Model
+//
+//  Created by CSUFTitan on 4/22/21.
+//
+
+import Foundation
+
+class Bank {
+  var bankClients: [String:Int]
+  var bankName: String
+  var totalBalance: Double
+  var phoneNumber: Int
+  var bankID: Int
+  init() {
+    bankClients = [:]
+    bankName = ""
+    totalBalance = 0
+    phoneNumber = 0
+    bankID = 0
+  }
+
+  func addBankClients(_ name: String, _ ID: Int) {
+    //for (key, value) in bankClients {
+    //}
+  }
+
+    func incBalance(_ deposit: Double) {
+    totalBalance += deposit
+  }
+    
+  func decBalance(_ withdraw: Double){
+    totalBalance -= withdraw
+  }
+
+}
